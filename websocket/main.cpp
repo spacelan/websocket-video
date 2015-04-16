9a9389702bb3d926484f0f4a8255a3165a63c762
SPACELAN <spacelan1993@gmail.com>
2015-04-16 16:26:19 +0800

    can specify device and resolution

--- websocket/main.cpp
+++ websocket/main.cpp
@@ -1,25 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "libwebsockets.h"
-#include <iostream>
 #include "pxcsensemanager.h"
 #include "pxcimage.h"
 #include "pxcsession.h"
 #include <windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
 #pragma comment(lib, "websockets.lib")
 
-struct RGBA
+class MyDeviceInfo: public PXCCapture::DeviceInfo
 {
-	unsigned char B, G, R, A;
+public:
+	std::vector<PXCCapture::Device::StreamProfileSet> profiles;
+	int numOfprofile;
 };
-unsigned char *buf = nullptr;
-int bufSize = 0;
+
+std::string wcharTochar(wchar_t *);
+void getDeviceInfo();
+
 int f = 0;
-unsigned int count = 0;
-bool received = false;
-//PXCSenseManager instance
-PXCSenseManager *sm = nullptr;
+std::vector<MyDeviceInfo> dinfo;
+int numOfDevice = 0;
+PXCSession *ss = nullptr;
 
 static int
 callback_http(struct libwebsocket_context * that,
@@ -36,11 +39,16 @@ struct libwebsocket *wsi,
 enum libwebsocket_callback_reasons reason,
 	void *user, void *in, size_t len)
 {
+	static unsigned char *buf = nullptr;
+	static int bufSize = 0;
+	static size_t count = 0;
+	static PXCSenseManager *sm = nullptr;
+
 	switch (reason) 
 	{
 	case LWS_CALLBACK_ESTABLISHED:
 	{
-		// just log message that someone is connecting
+		printf("connection established\n");
 		f = 0;
 		count = 0;
 		if (buf)
@@ -49,7 +57,8 @@ enum libwebsocket_callback_reasons reason,
 		if (sm)
 			sm->Release();
 		sm = nullptr;
-		printf("connection established\n");
+		sm = ss->CreateSenseManager();
+
 		break;
 	}
 	case LWS_CALLBACK_SERVER_WRITEABLE: 
@@ -58,7 +67,7 @@ enum libwebsocket_callback_reasons reason,
 		SYSTEMTIME time;
 		int time_ms;
 		unsigned char *p = buf + LWS_SEND_BUFFER_PRE_PADDING;
-		if (f > 3){
+		if (f > 1){
 			GetLocalTime(&time);
 			time_ms = time.wMilliseconds + (time.wSecond + (time.wMinute + time.wHour * 60) * 60) * 1000;
 			for (int i = 0; i < bufSize; i += 4){
@@ -112,88 +121,32 @@ enum libwebsocket_callback_reasons reason,
 	case LWS_CALLBACK_RECEIVE:
 	{
 		f = atoi((const char*)in);
+		if (buf)
+			delete buf;
+		buf = nullptr;
+		sm->Close();
 		switch (f)
 		{
 		case 0:
 		{
-				  if (buf)
-					  delete buf;
-				  buf = nullptr;
-				  if (sm)
-					  sm->Release();
-				  sm = nullptr;
-				  break;
+			break;
 		}
 		case 1:
 		{
-				  if (buf)
-					  delete buf;
-				  buf = nullptr;
-				  if (sm)
-					  sm->Release();
-				  sm = nullptr;
-
-				  bufSize = 640 * 480 * 4;
-				  buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
-				  sm = PXCSenseManager::CreateInstance();
-				  // Select the color stream
-				  sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 640, 480);
-				  // Initialize and Stream Samples
-				  sm->Init();
-				  break;
-		}
-		case 2:
-		{
-				  if (buf)
-					  delete buf;
-				  buf = nullptr;
-				  if (sm)
-					  sm->Release();
-				  sm = nullptr;
-
-				  bufSize = 800 * 600 * 4;
-				  buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
-				  sm = PXCSenseManager::CreateInstance();
-				  // Select the color stream
-				  sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 800, 600);
-				  // Initialize and Stream Samples
-				  sm->Init();
-				  break;
-		}
-		case 3:
-		{
-				  if (buf)
-					  delete buf;
-				  buf = nullptr;
-				  if (sm){
-					  sm->Release();
-					  sm;
-				  }
-				  sm = nullptr;
-
-				  bufSize = 1280 * 960 * 4;
-				  buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
-				  sm = PXCSenseManager::CreateInstance();
-				  // Select the color stream
-				  sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 1280, 960);
-				  // Initialize and Stream Samples
-				  sm->Init();
-				  break;
+			sm->QueryCaptureManager()->FilterByDeviceInfo(&dinfo[numOfDevice]);
+			int width = dinfo[numOfDevice].profiles[dinfo[numOfDevice].numOfprofile].color.imageInfo.width;
+			int height = dinfo[numOfDevice].profiles[dinfo[numOfDevice].numOfprofile].color.imageInfo.height;
+			sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, width, height);
+			bufSize = width * height * 4;
+			buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
+			sm->Init();
+			break;
 		}
 		default:
 		{
-				   if (buf)
-					   delete buf;
-				   buf = nullptr;
-				   if (sm){
-					   sm->Release();
-					   sm;
-				   }
-				   sm = nullptr;
-
-				   bufSize = f * 4;
-				   buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
-				   break;
+			bufSize = f * 4;
+			buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
+			break;
 		}
 		}
 		break;
@@ -203,7 +156,97 @@ enum libwebsocket_callback_reasons reason,
 		std::cout << "closed" << std::endl;
 		f = false;
 		count = 0;
+		if (buf)
+			delete buf;
+		buf = nullptr;
+		if (sm)
+			sm->Release();
+		sm = nullptr;
 		break;
+	}
+	default:
+		break;
+	}
+	return 0;
+}
+
+static int
+callback_camera_info(struct libwebsocket_context * that,
+struct libwebsocket *wsi,
+enum libwebsocket_callback_reasons reason,
+	void *user, void *in, size_t len)
+{
+	char printBuf[20];
+	unsigned char *buf;
+	unsigned char *p;
+	size_t bufSize;
+	switch (reason)
+	{
+	case LWS_CALLBACK_ESTABLISHED:
+	{
+		getDeviceInfo();
+		if (dinfo.size()){
+			sprintf_s(printBuf, "name");
+			std::string nameToSend(printBuf);
+			for (size_t i = 0; i < dinfo.size(); i++){
+				std::string name = wcharTochar(dinfo[i].name);
+				nameToSend += "," + name;
+			}
+			bufSize = nameToSend.length();
+			buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
+			p = buf + LWS_SEND_BUFFER_PRE_PADDING;
+			memcpy(p, nameToSend.c_str(), bufSize);
+			libwebsocket_write(wsi, p, bufSize, LWS_WRITE_TEXT);
+			delete buf;
+			buf = p = nullptr;
+			
+			sprintf_s(printBuf, "profile");
+			std::string profileToSend(printBuf);
+			for (size_t i = 0; i < dinfo[0].profiles.size(); i++){
+				sprintf_s(printBuf, ",%dx%d", dinfo[0].profiles[i].color.imageInfo.width, dinfo[0].profiles[i].color.imageInfo.height);
+				profileToSend.append(printBuf);
+			}
+			bufSize = profileToSend.length();
+			buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
+			p = buf + LWS_SEND_BUFFER_PRE_PADDING;
+			memcpy(p, profileToSend.c_str(), bufSize);
+			libwebsocket_write(wsi, p, bufSize, LWS_WRITE_TEXT);
+			delete buf;
+			buf = p = nullptr;
+		}
+		numOfDevice = 0;
+		break;
+	}
+	case LWS_CALLBACK_SERVER_WRITEABLE:
+	{
+
+	}
+	case LWS_CALLBACK_RECEIVE:
+	{
+		int num = atoi((const char*)in);
+		if (num < 10){
+			numOfDevice = num;
+			sprintf_s(printBuf, "profile");
+			std::string profileToSend(printBuf);
+			for (size_t i = 0; i < dinfo[numOfDevice].profiles.size(); i++){
+				sprintf_s(printBuf, ",%dx%d", dinfo[numOfDevice].profiles[i].color.imageInfo.width, dinfo[numOfDevice].profiles[i].color.imageInfo.height);
+				profileToSend.append(printBuf);
+			}
+			bufSize = profileToSend.length();
+			buf = new unsigned char[bufSize + LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING];
+			p = buf + LWS_SEND_BUFFER_PRE_PADDING;
+			memcpy(p, profileToSend.c_str(), bufSize);
+			libwebsocket_write(wsi, p, bufSize, LWS_WRITE_TEXT);
+			delete buf;
+			buf = p = nullptr;
+		}
+		else{
+			dinfo[numOfDevice].numOfprofile = num - 10;
+		}
+	}
+	case LWS_CALLBACK_CLOSED:
+	{
+
 	}
 	default:
 		break;
@@ -224,9 +267,53 @@ static struct libwebsocket_protocols protocols[] = {
 		0                          // we don't use any per session data
 	},
 	{
+		"camera-info",
+		callback_camera_info,
+		0
+	},
+	{
 		NULL, NULL, 0   /* End of list */
 	}
 };
+
+void getDeviceInfo()
+{
+	PXCSession::ImplDesc desc1 = {};
+	desc1.group = PXCSession::IMPL_GROUP_SENSOR;
+	desc1.subgroup = PXCSession::IMPL_SUBGROUP_VIDEO_CAPTURE;
+	PXCSession::ImplDesc desc2;
+
+	dinfo.clear();
+
+	if (ss->QueryImpl(&desc1, 0, &desc2) >= PXC_STATUS_NO_ERROR){
+		PXCCapture *capture;
+		ss->CreateImpl<PXCCapture>(&desc2, &capture);
+		for (int d = 0;; d++) {
+			MyDeviceInfo info;
+			if (capture->QueryDeviceInfo(d, &info) < PXC_STATUS_NO_ERROR) break;
+			PXCCapture::Device *device = capture->CreateDevice(d);
+			for (int p = 0;; p++){
+				PXCCapture::Device::StreamProfileSet profile = {};
+				if (device->QueryStreamProfileSet(PXCCapture::STREAM_TYPE_COLOR, p, &profile) < PXC_STATUS_NO_ERROR) break;
+				info.profiles.push_back(profile);
+				info.numOfprofile = 0;
+			}
+			dinfo.push_back(info);
+		}
+		capture->Release();
+	}
+}
+
+std::string wcharTochar(wchar_t *w)
+{
+	size_t len = wcslen(w) + 1;
+	size_t converted = 0;
+	char *CStr = new char[len];
+	wcstombs_s(&converted, CStr, len, w, _TRUNCATE);
+	std::string str(CStr);
+	delete CStr;
+	return str;
+}
 
 int main(void) {
 	struct libwebsocket_context *context;
@@ -250,6 +337,7 @@ int main(void) {
 
 	printf("starting server...\n");
 
+	ss = PXCSession::CreateInstance();
 	// infinite loop, to end this server send SIGTERM. (CTRL+C)
 	while (1) 
 	{
@@ -257,6 +345,7 @@ int main(void) {
 			libwebsocket_callback_on_writable_all_protocol(&protocols[1]);
 		libwebsocket_service(context, 10);
 	}
+	ss->Release();
 	libwebsocket_context_destroy(context);
 	system("pause");
 	return 0;
